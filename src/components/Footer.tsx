c6b0153fac7520a1fdd9ad8b0547520a0d279283
mgavillo <marie.gavillon@gmail.com>
2023-08-21 19:07:02 +0100

    updated socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,16 @@
 import { SimpleSeparator } from "./Separator";
-import { BsTwitter, BsYoutube } from "react-icons/bs";
+import { BsLinkedin, BsTwitter, BsYoutube } from "react-icons/bs";
 export default function Footer() {
   return (
     <footer className="">
       <div className="flex flex-row gap-4">
-        <BsTwitter size={30} />
-        <BsYoutube size={30} />
+        <a href="https://twitter.com/gaiaconsortium" target="_blank">
+          <BsTwitter size={30} />
+        </a>
+        {/* <BsYoutube size={30} /> */}
+        <a href="https://www.linkedin.com/company/gaia-consortium/" target="_blank">
+          <BsLinkedin size={30} />
+        </a>
       </div>
       <SimpleSeparator />
       <h2>Gaia Consortium</h2>
