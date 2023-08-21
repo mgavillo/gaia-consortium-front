b0f8394bacf25ff6bc51251e55346c2f14d8410c
Rafael Kaufmann <rafael.kaufmann@gmail.com>
2023-08-21 18:26:38 +0100

    Update CTA.tsx

--- src/app/sections/CTA.tsx
+++ src/app/sections/CTA.tsx
@@ -22,11 +22,11 @@ export default function CTA() {
 
   return (
     <section className="">
-      <h2 className="w-full text-center">Enter the Realm of Collaboration</h2>
+      <h2 className="w-full text-center">Let's do this together</h2>
       <div className="w-full flex flex-row flex-wrap  md:flex-nowrap gap-12 ">
         <div className="w-full h-56 flex flex-col items-start justify-center rounded-md p-8 bg-slate-800 gap-6 border border-gray-800 bg-opacity-30 min-w-[330px]">
           <h3>Stay Informed</h3>
-          <p>Subscribe to our newsletter to get the updates of development of the Gaia Consurtium</p>
+          <p>Subscribe to our newsletter to get news and updates about the Gaia Consortium</p>
           <EmailForm />
         </div>
         <span className="h-1 w-full md:w-2 md:h-56 relative">
@@ -34,10 +34,10 @@ export default function CTA() {
           <OrSeparator orientation={isMobile ? "vertical" : "horizontal"} />
         </span>
         <div className="w-full h-56 flex flex-col items-start justify-center rounded-md p-8 gap-6 bg-slate-800 border border-gray-800 bg-opacity-30 min-w-[330px]">
-          <h3>Join the team</h3>
-          <p>You or your team think they can grow alongside us ? </p>
+          <h3>Join the Consortium</h3>
+          <p>Want to help us build the Consortium, as an individual or organization? </p>
           <div className="w-full flex flex-row justify-end">
-            <Button text="Take the survey" />
+            <Button text="Get in touch!" />
           </div>
         </div>
       </div>
