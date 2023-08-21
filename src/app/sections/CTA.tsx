f1f72ef0c970d7467067af1f022403ea3fa88e65
Rafael Kaufmann <rafael.kaufmann@gmail.com>
2023-08-21 18:28:54 +0100

    Update CTA.tsx

--- src/app/sections/CTA.tsx
+++ src/app/sections/CTA.tsx
@@ -22,7 +22,7 @@ export default function CTA() {
 
   return (
     <section className="">
-      <h2 className="w-full text-center">Let's do this together</h2>
+      <h2 className="w-full text-center">Let&apos;s do this together</h2>
       <div className="w-full flex flex-row flex-wrap  md:flex-nowrap gap-12 ">
         <div className="w-full h-56 flex flex-col items-start justify-center rounded-md p-8 bg-slate-800 gap-6 border border-gray-800 bg-opacity-30 min-w-[330px]">
           <h3>Stay Informed</h3>
