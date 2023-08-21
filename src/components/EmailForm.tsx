103a60b7594d4b53e94f54cf91ab726ef35db9cf
mgavillo <marie.gavillon@gmail.com>
2023-08-21 15:26:32 +0100

    add orientation for email from

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,10 +1,10 @@
 import Button from "./Button";
 
-export default function EmailForm(){
-    return(
-        <form className="flex flex-row gap-4 w-full">
-            <input className="px-6 py-3 rounded-sm bg-slate-900 w-full" placeholder="Your email"></input>
-            <Button text="Subscribe"/>
-        </form>
-    )
+export default function EmailForm({ orientation = "horizontal" }: { orientation?: "horizontal" | "vertical" }) {
+  return (
+    <form className={`flex ${orientation == "horizontal" ? "flex-row" : "flex-col"} gap-4 w-full`}>
+      <input className="px-6 py-3 rounded-sm bg-slate-900 w-full" placeholder="Your email"></input>
+      <Button text="Subscribe" />
+    </form>
+  );
 }
