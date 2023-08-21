53e239553e3082f583b4664e8e6a9df2aa920e96
Rafael Kaufmann <rafael.kaufmann@gmail.com>
2023-08-21 18:48:01 +0100

    Update Partners.tsx

--- src/app/sections/Partners.tsx
+++ src/app/sections/Partners.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Partners() {
   return (
     <section id="Partners" className="w-full h-fit gap-8 -mx-24  border-slate-800 bg-opacity-40 p-0 md:p-8">
-      <p className=" text-slate-600 tracking-wider text-center italic font-serif">A partnership between sector teams</p>
+      <p className=" text-slate-600 tracking-wider text-center italic font-serif">An open alliance between:</p>
       <div className="flex flex-row flex-wrap gap-4 md:gap-12 w-full items-center justify-center">
         <Image src={dgLogo} alt="Logo of Digital gaia" className="h-6 md:h-12 w-auto" />
         <Image src={timelikeLogo} alt="Logo of Timelike" className="h-6 md:h-12 w-auto" />
