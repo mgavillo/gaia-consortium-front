6bbcaf2614c1098dfde3da120160853827af9ca2
mgavillo <marie.gavillon@gmail.com>
2023-08-21 15:26:51 +0100

    add hamburger menu for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@
 import { useEffect, useRef, useState } from "react";
 import EmailForm from "./EmailForm";
 export default function Header() {
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState<boolean>(false);
+
   function onLinkClick(e: React.MouseEvent, target: string) {
     let el = document.getElementById(target);
     e.preventDefault(); // Stop Page Reloading
@@ -12,7 +14,7 @@ export default function Header() {
   }
 
   return (
-    <div className={`w-screen fixed z-50 top-0 border-b-2 border-gray-900 font-bold text-xl`}>
+    <nav className={`w-screen fixed z-50 top-0 border-b-2 border-gray-900 font-bold text-xl`}>
       {/* <div className="w-full h-8 bg-darkBlue text-center"> We are searching funding, click here</div> */}
       <div className="w-full h-20 bg-black/80 backdrop-blur-md flex flex-row justify-between items-center px-8">
         <div className="w-96">
@@ -20,7 +22,16 @@ export default function Header() {
             Gaia Consortium
           </a>
         </div>
-        <div className="flex flex-row gap-8">
+
+        <div className="lg:hidden relative">
+          <button className="navbar-burger flex items-center text-skyBlue p-3" onClick={() => setIsHamburgerOpen(true)}>
+            <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <title>Mobile menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+            </svg>
+          </button>
+        </div>
+        <div className="flex-row gap-8 hidden lg:flex">
           <a onClick={(e) => onLinkClick(e, "Partners")} className=" cursor-pointer">
             Partners
           </a>
@@ -31,10 +42,56 @@ export default function Header() {
             Principles
           </a>
         </div>
-        <div className=" w-96 font-normal text-base">
+        <div className=" w-96 font-normal text-base hidden lg:flex">
           <EmailForm />
         </div>
       </div>
-    </div>
+      {isHamburgerOpen && (
+        <div className="bg-black rounded-md p-4 absolute inset-0 -right-12 flex flex-col gap-16 w-screen h-screen">
+          <button
+            className="w-full text-right font-normal px-8 py-2 text-skyBlue"
+            onClick={() => setIsHamburgerOpen(false)}>
+            X
+          </button>
+          <div className="flex-col gap-8 flex lg:hidden  ">
+            <a
+              onClick={(e) => {
+                setIsHamburgerOpen(false);
+                onLinkClick(e, "Gaia-consortium");
+              }}
+              className="cursor-pointer">
+              Gaia Consortium
+            </a>
+            <a
+              onClick={(e) => {
+                setIsHamburgerOpen(false);
+                onLinkClick(e, "Partners");
+              }}
+              className=" cursor-pointer">
+              Partners
+            </a>
+            <a
+              onClick={(e) => {
+                setIsHamburgerOpen(false);
+                onLinkClick(e, "Metacrisis");
+              }}
+              className="cursor-pointer">
+              Metacrisis
+            </a>
+            <a
+              onClick={(e) => {
+                setIsHamburgerOpen(false);
+                onLinkClick(e, "Principles");
+              }}
+              className="cursor-pointer">
+              Principles
+            </a>
+          </div>
+          <span className="font-normal w-full">
+            <EmailForm orientation="vertical" />
+          </span>
+        </div>
+      )}
+    </nav>
   );
 }
