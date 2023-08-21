2630a7dbd5de52387621f9d531dc1522a3035011
mgavillo <marie.gavillon@gmail.com>
2023-08-21 15:53:12 +0100

    responsive quote

--- src/app/sections/Quote.tsx
+++ src/app/sections/Quote.tsx
@@ -1,13 +1,13 @@
 export default function Quote() {
   return (
-    <section className="py-24 flex flex-col gap-4">
+    <section className="py-24 flex flex-col gap-16">
       {/* <span className=" -mb-12 ">
         <BiSolidQuoteAltRight
           size={100}
           className={"bg-gradient-to-tr from-lightBlue to-darkBlue text-transparent bg-clip-text"}
         />
       </span> */}
-      <p className="w-full font-black text-6xl text-center">
+      <p className="w-full font-black text-2xl lg:text-6xl text-center">
         We might find ways to ground human decision-making in the basic evolutionary unfolding of the world.
       </p>
       {/* <p className=" w-full font-bold text-8xl">
@@ -18,14 +18,14 @@ export default function Quote() {
         </span>
         <span className=" italic">(the basic premise of democracy)</span>, we might look for ways to listen in to
       </p> */}
-      <div className="flex flex-col text-4xl">
-        <div className="flex flex-row items-center gap-4 font-black">
-          <p className="w-[700px] text-right  text-neutral-600">Rather than projecting</p>
-          <p className="w-[700px] font-black stroke-red"> what people want onto the world</p>
+      <div className="flex flex-col text-base xl:text-4xl font-medium gap-4">
+        <div className="flex flex-col xl:flex-row items-center justify-center gap-1 lg:gap-4 ">
+          <p className="w-[700px] text-center xl:text-right text-neutral-600">Rather than projecting</p>
+          <p className="w-[700px] text-center xl:text-left text-2xl lg:text-4xl font-black"> what people want onto the world</p>
         </div>
-        <div className="flex flex-row items-center gap-4 text-4xl font-black">
-          <p className="w-[700px]  text-right text-neutral-600">We might look for ways to listen in to</p>
-          <p className=" w-[700px] font-black  "> what the world is calling for.</p>
+        <div className="flex flex-col xl:flex-row items-center justify-center gap-1 lg:gap-4 ">
+          <p className="w-[700px] text-center xl:text-right text-neutral-600">We might look for ways to listen in to</p>
+          <p className=" w-[700px] text-center xl:text-left text-2xl lg:text-4xl font-black "> what the world is calling for.</p>
         </div>
       </div>
     </section>
