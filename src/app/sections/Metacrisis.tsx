371ea734b68967d2957f9e4514017e0ab372bf63
Rafael Kaufmann <rafael.kaufmann@gmail.com>
2023-08-21 18:55:14 +0100

    Update Metacrisis.tsx

--- src/app/sections/Metacrisis.tsx
+++ src/app/sections/Metacrisis.tsx
@@ -8,9 +8,10 @@ export default function MetaCrisis() {
       <Image src={Bg01} alt="" className="absolute -left-[700px] -top-[100px] blur-[1100px] -z-40" width={8000} />
       <h2>the Metacrisis</h2>
       <p className=" w-full md:w-1/2 text-center">
-        Think of the Metacrisis as a collision of global crises – ecological, psychological, cultural, economic – each
-        contributing its own urgency. This isn&apos;t just a crisis blend; it&apos;s a new crisis altogether that demands a
-        multi-pronged approach.
+        Think of the Metacrisis as a collision of global crises – ecological, psychological, cultural, economic – that combine
+        to create total risk for our species and planet. What awaits us on the other side of the Metacrisis – if we survive it:
+        a Third Attractor where our global system is intentionally and wisely managed at every scale, creating lasting prosperity
+        and resilience for all.
       </p>
     </section>
   );
