4387c170b943f5b06ca829de3300cd4eba5ab36b
mgavillo <marie.gavillon@gmail.com>
2023-08-21 16:53:37 +0100

    fix server side with tailwind class

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,16 +1,28 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export function SimpleSeparator() {
   return <hr className=" w-full h-[1px] bg-gradient-radial from-lightBlue to-darkBlue border-0" />;
 }
 
 export function OrSeparator({ orientation = "vertical" }: { orientation?: "horizontal" | "vertical" }) {
-  console.log(orientation);
+  const [mounted, setMounted] = useState(false);
 
-  const hrStyle = `${orientation == "vertical" ?  "h-[1px] w-1/2" : "w-[1px] h-1/2"} bg-slate-800 border-0`;
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+  const hrStyle = `${orientation == "vertical" ? "h-[1px] w-1/2" : "w-[1px] h-1/2"} bg-slate-800 border-0`;
+
   return (
     <div className={`flex ${orientation == "vertical" ? "flex-row" : "flex-col"} h-full items-center gap-4`}>
-      <hr className={hrStyle} />
+      <hr className={`${hrStyle}`} />
       <p>OR</p>
-      <hr className={hrStyle} />
+      <hr className={`${hrStyle}`} />
     </div>
   );
 }
