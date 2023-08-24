2ba7b2767804ae36a7438eadb8bafe5088c4cfb3
mgavillo <marie.gavillon@gmail.com>
2023-08-24 22:27:26 +0100

    seo optimization

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button({ text, type = "button" }: { text: string; type?: "button" | "submit" | "reset" }) {
   return (
-    <button className=" rounded-sm bg-skyBlue px-6 py-3" type={type}>
+    <button className=" rounded-sm bg-darkBlue px-6 py-3 hover:bg-opacity-90 transition-all" type={type}>
       {text}
     </button>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,11 @@ export default function Footer() {
   return (
     <footer className="">
       <div className="flex flex-row gap-4">
-        <a href="https://twitter.com/gaiaconsortium" target="_blank">
+        <a href="https://twitter.com/gaiaconsortium" aria-label="Follow us on Twitter" target="_blank">
           <BsTwitter size={30} />
         </a>
         {/* <BsYoutube size={30} /> */}
-        <a href="https://www.linkedin.com/company/gaia-consortium/" target="_blank">
+        <a href="https://www.linkedin.com/company/gaia-consortium/" aria-label="Follow us on Linkedin" target="_blank">
           <BsLinkedin size={30} />
         </a>
       </div>
